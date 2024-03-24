16cb4d8c129b4fe40a605437d203d4a05584a2d9
Patrick Lumumba <97313939+lumumbapl@users.noreply.github.com>
2024-03-24 17:28:21 +0300

    show admin message

--- footnotes-made-easy.php
+++ footnotes-made-easy.php
@@ -596,3 +596,17 @@ class swas_wp_footnotes {
 		wp_enqueue_style( 'wp-footnotes-tt-style', plugins_url( 'css/tooltips.min.css' , __FILE__ ), array(), null );
 	}
 }
+
+/**
+ * Show Admin Message
+ *
+ * Display message on the administration screen.
+ */ 
+function fme_add_admin_notice() {
+    if (is_admin()) {
+        echo '<div class="notice notice-error"><p>';
+        echo __( sprintf( '⚠️The Footnotes Made Easy plugin will be discontinued December 2024. After this time there will be no further updates, including security vulnerabilities. It is important that you disable it and find an alternative plugin before then. <a href="%s">Find out more here</a>.', 'https://wordpress.org/support/topic/important-please-read-before-posting-4/' ), 'footnotes-made-easy' );
+        echo '</p></div>';
+    }
+}
+add_action( 'admin_notices', 'fme_add_admin_notice' );
